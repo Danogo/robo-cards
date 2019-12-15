2425fb3d2b6320f057d5e84039074c72e735dba6
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2019-12-15 10:33:36 +0100

    Updates

---
+++ precache-manifest.e589111ca0176a5e4a8180bd6ad3a45a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb0dcec620a6eeaa7a9e860a518a412",
+    "url": "/robo-cards/index.html"
+  },
+  {
+    "revision": "b3f83d1a1d8bb2f8c346",
+    "url": "/robo-cards/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "747ae2c66ab64c83594a",
+    "url": "/robo-cards/static/css/main.6647ec88.chunk.css"
+  },
+  {
+    "revision": "b3f83d1a1d8bb2f8c346",
+    "url": "/robo-cards/static/js/2.975ee9db.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/robo-cards/static/js/2.975ee9db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "747ae2c66ab64c83594a",
+    "url": "/robo-cards/static/js/main.9b2c1e97.chunk.js"
+  },
+  {
+    "revision": "1a2d01c9ec9d4497e2ca",
+    "url": "/robo-cards/static/js/runtime-main.c74f921c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-cards/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robo-cards/precache-manifest.8af734c3913b3ad45569829a0612039a.js"
+  "/robo-cards/precache-manifest.e589111ca0176a5e4a8180bd6ad3a45a.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/robo-cards/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robo-cards/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
